598af923c16f8a2b75b10e18cdeb015dfe6e979c
Thiago Maturana <thiagomatu@gmail.com>
2019-02-11 11:49:52 -0200

    initial commit

---
+++ src/styles/global.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+import { createGlobalStyle } from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: #181818;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: 'Montserrat', sans-serif;
+    color: #FFF;
+  }
+`;
+
+export default GlobalStyle;
